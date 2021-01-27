88cd21fff64e98c514b2c993deb5be44ae625e95
USER <USER@HP>
2021-01-27 17:16:35 +0800

    v01

---
+++ mavenproject01/src/main/java/mavenproject01/MyMain.java
@@ -0,0 +1,12 @@
+package mavenproject01;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Working..");
+		System.out.println("Working..1");
+		
+	}
+
+}
